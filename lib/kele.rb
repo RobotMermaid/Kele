f4a3c2abc0a0d0f1bfa895f41e8c21cf61e6877b
RobotMermaid <ondinerangel@gmail.com>
2017-01-03 12:07:21 -0800

    retrieve own data and convert to Ruby hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,16 +1,23 @@
-require "httparty"
+require 'httparty'
+require 'json'
 
 class Kele
-  #include HTTParty
+  include HTTParty
+  base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-      @url= 'https://www.bloc.io/api/v1/sessions'
-
-      response = HTTParty.post(@url,
+      response = self.class.post('/sessions',
          :query => { :email => email, :password => password })
-      puts response
-
-      raise "invalid email/pass, bud" if response.code != 200
-
+      #puts response
+      @auth_token = response["auth_token"]
+      raise "this is an invalid email/password" if response.code != 200
   end
+
+   def get_me
+      url= 'https://www.bloc.io/api/v1/users/me'
+      response = self.class.get(url, headers: { "authorization" => @auth_token })
+      #puts response["bio"]
+      JSON.parse(response.body)
+      #puts JSON.pretty_generate(response)
+   end
 end

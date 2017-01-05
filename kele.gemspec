42bfe00ff938fc74064e8f61084fbf9fcba51150
RobotMermaid <ondinerangel@gmail.com>
2017-01-05 11:11:14 -0800

    roadmaps and checkpoints in a seperate module

--- kele.gemspec
+++ kele.gemspec
@@ -6,11 +6,12 @@ Gem::Specification.new do |s|
   s.description   = 'A client for the Bloc API'
   s.authors       = ['Ondine']
   s.email         = 'ondinerangel@gmail.com'
-  s.files         = ['lib/kele.rb']
+  s.files         = ['lib/kele.rb', "lib/kele/roadmap.rb"]
   s.require_paths = ["lib"]
   s.homepage      =
     'http://rubygems.org/gems/kele'
   s.license       = 'MIT'
   s.add_runtime_dependency 'httparty', '~> 0.13'
   s.add_runtime_dependency 'json', '~> 1.8'
+  s.add_runtime_dependency 'pry'
 end
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,11 @@
 require 'httparty'
 require 'json'
+require_relative 'kele/roadmap'
 
 class Kele
   include HTTParty
+  include Roadmap
+
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
@@ -24,6 +27,24 @@ class Kele
    def get_mentor_availability(mentor_id)
      response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
      #Convert the JSON response to a Ruby array.
+     JSON.parse(response.body)
+   end
+
+   def get_messages
+     response = self.class.get('/message_threads',
+     headers: { "authorization" => @auth_token })
+     JSON.parse(response.body)
+   end
+
+   def create_message(sender, recipient_id, subject, stripped_text)
+     response = self.class.post('/messages',
+     headers: { "authorization" => @auth_token },
+     "sender"=> sender,
+     "recipient_id"=> recipient_id,
+     "subject"=> subject,
+     "stripped-text"=> stripped_text
+     )
+     puts response
      JSON.parse(response.body)
    end
 end
